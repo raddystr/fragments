74a3ce5038435193a1accd63b658aa2b2e579517
RadiRadev <raddy.radev@gmail.com>
2018-05-31 08:16:24 +0300

    some new code

---
+++ SplitWordsByLowerUpperorMixedCase.java
@@ -0,0 +1,48 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+
+        String [] words=console.nextLine()
+                .split( "[\\\\,;:.!()\"\'/\\[\\] ]");
+
+        List<String>lowerCase =new ArrayList<>();
+        List<String>upperCase =new ArrayList<>();
+        List<String>mixedCase =new ArrayList<>();
+
+        for (String word : words) {
+            if(word.trim().length()==0) {
+                continue;
+            }
+            boolean isMixed=false;
+            for (int i = 0; i <word.length() ; i++) {
+                if(!Character.isLetter(word.charAt(i))){
+                    mixedCase.add(word);
+                    isMixed = true;
+                    break;
+                }
+            }
+            if (!isMixed){
+                if (word.toUpperCase().equals(word)){
+                    upperCase.add(word);
+                }else if (word.toLowerCase().equals(word)){
+                    lowerCase.add(word);
+                }else {
+                    mixedCase.add(word);
+                }
+                    }
+                }
+        System.out.printf("Lower-case: %s%n", String.join(", ", lowerCase));
+        System.out.printf("Mixed-case: %s%n", String.join(", ", mixedCase));
+        System.out.printf("Upper-case: %s%n", String.join(", ", upperCase));
+             }
+        }
+
+
