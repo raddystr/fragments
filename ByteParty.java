11f96f4815d7e74c05fc848c9a317ac598c9c088
RadiRadev <raddy.radev@gmail.com>
2018-07-09 09:30:40 +0300

    some code

---
+++ ByteParty.java
@@ -0,0 +1,76 @@
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner console = new Scanner(System.in);
+
+        int countOfNumbers = Integer.parseInt(console.nextLine());
+
+        int[] numbers = new int[countOfNumbers];
+
+        for (int i = 0; i < countOfNumbers; i++) {
+            int number = Integer.parseInt(console.nextLine());
+            numbers[i] = number;
+
+        }
+
+        String line;
+        while (true) {
+            if ("party over".equals(line = console.nextLine())) {
+                break;
+            }
+                String[] tokens = line.split("\\s+");
+                int pattern = Integer.parseInt(tokens[0]);
+                int position = Integer.parseInt(tokens[1]);
+
+                for (int i = 0; i < numbers.length; i++) {
+                    switch (pattern) {
+                        case 1:
+                           changeValueToOne(numbers, position);
+                            break;
+                        case 0:
+                            chageValueToZero(numbers, position);
+                            break;
+                        case -1:
+                            changeValueByPosition(numbers,position,i);
+                            break;
+                    }
+
+                }
+            }
+        for (int number : numbers) {
+            System.out.println(number);
+
+            }
+        }
+        private static void chageValueToZero(int[] numbers, int postion){
+            for (int i = 0; i <numbers.length ; i++) {
+            int number = numbers[i];
+            int mask = ~(1<<postion);
+            numbers[i] = number & mask;
+            }
+        }
+        private static void changeValueToOne(int[] numbers, int position){
+            for (int i = 0; i <numbers.length ; i++) {
+                int number = numbers[i];
+                int mask = 1 <<position;
+                numbers [i] = number | mask;
+            }
+        }
+        private static void changeValueByPosition(int [] numbers, int position, int i){
+         int number = numbers[i];
+         int numberAfterShift = number>>position;
+         int firstBit = numberAfterShift &1;
+         if(firstBit ==0){
+            int mask = 1<<position;
+            numbers[i] = number | mask;
+         }else {
+             int mask = ~(1<<position);
+             numbers[i] = number & mask;
+            }
+        }
+    }
+
