334bdc1405f7103108a4916b8fc4ac599f15e270
Radi Radev <33155474+RSRadev@users.noreply.github.com>
2019-02-26 17:13:24 +0200

    Update BinaryConverter.java

--- BinaryConverter.java
+++ BinaryConverter.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
-
         int number = Integer.parseInt(console.nextLine());
         ArrayDeque<Integer>stack  = new ArrayDeque<>();
 
