524481abc37e2ab566e39fda606feea3f5ba923f
Radi Radev <33155474+RSRadev@users.noreply.github.com>
2019-03-01 16:21:27 +0200

    Update arrOps.pl

--- arrOps.pl
+++ arrOps.pl
@@ -6,10 +6,13 @@ use warnings;
 my @arr = qw(Petko Metko Zetko Detko);
 
 my $shifty = shift @arr;
+
 my $popy = pop @arr;
 
 unshift (@arr, "Cvetko");
+
 push (@arr, "Tetko");
+
 print $shifty. "\n";
 print $popy ."\n";
 
