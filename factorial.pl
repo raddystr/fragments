dbf968a52d24f16c5e2d6858c2289fdc0c049236
RSRadev <raddy.radev@gmail.com>
2019-03-01 12:34:25 +0200

    factorial

---
+++ factorial.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+# Complete the factorial function below.
+sub factorial {
+    my($num)=@_;
+    if($num==1){
+        return 1;
+    }else{
+        return $num*factorial($num-1);
+        }
+    }
+
+
+open(my $fptr, '>', $ENV{'OUTPUT_PATH'});
+
+my $n = <>;
+$n =~ s/\s+$//;
+
+my $result = factorial($n);
+
+print $fptr "$result\n";
+
+close $fptr;
