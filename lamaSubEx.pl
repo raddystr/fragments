abfad7fe9df848539451e1d1386f0a63fec6d0d5
RSRadev <raddy.radev@gmail.com>
2019-03-05 10:51:15 +0200

    ex

---
+++ lamaSubEx.pl
@@ -0,0 +1,26 @@
+#!/usr/local/bin/perl
+use strict;
+use warnings;
+
+sub sum{
+	my $sum;
+	my $number;
+	my @numbers = @_;
+	my $count = $#numbers+1;
+		foreach $number(@numbers){
+		$sum +=$number;
+}
+		my $average = $sum/$count;
+		
+	foreach $number(@numbers){
+		if($number>$average){
+			print"$number\n";
+		}
+	}
+			#print"$sum\n";
+			#print"$count\n";
+			#print"$average\n"; 
+			
+		
+}		
+sum(1...1000)
