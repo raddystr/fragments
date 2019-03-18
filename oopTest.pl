22c1a7fb9a46cc151fc0800a54ce85303c25d202
RSRadev <raddy.radev@gmail.com>
2019-03-18 12:32:59 +0200

    oop Test

---
+++ oopTest.pl
@@ -0,0 +1,18 @@
+use strict;
+use lib '/usr/home/raddy/projects/OOP/'; 
+use Quote;
+
+my $phrase = "Baz";
+my $author = "Foo";
+my $approved = 0;
+
+my $quote = Quote->new();
+$quote->set_phrase($phrase);
+$quote->set_author($author);
+$quote->is_approved($approved);
+
+print STDOUT $quote->get_phrase(), "\n";
+print STDOUT $quote->get_author(), "\n";
+print STDOUT ($quote->is_approved() ? "Is approved": "Is not approved"), "\n";
+
+exit 0;
