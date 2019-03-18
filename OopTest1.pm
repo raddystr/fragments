1808066229e6c33f67be7f1962770a84bb19f904
RSRadev <raddy.radev@gmail.com>
2019-03-18 12:37:18 +0200

    package

---
+++ OopTest1.pm
@@ -0,0 +1,34 @@
+package Quote;
+
+use strict;
+
+sub new{
+	my $class = shift;
+	my $self = {};
+	return bless $self, $class;
+}
+
+sub set_phrase{
+	my $self = shift;
+	my $phrase = shift;
+	$self->{phrase} = $phrase;
+}
+sub get_phrase{
+	my $self = shift;
+	return $self->{phrase};
+}
+sub set_author {
+	my $self = shift;
+	my $author = shift;
+	$self->{author} = $author;
+}
+sub get_author {
+	my $self = shift;
+	return $self->{author};
+}
+
+sub is_approved{
+	my $self = shift;
+	@_ ? $self->{approved} = shift : $self->{approved}; 
+}
+1;
