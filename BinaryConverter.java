e9038873ce0795bb7d461c2e6cc48f9eecfaa747
Radi Radev <33155474+RSRadev@users.noreply.github.com>
2019-03-20 21:48:23 +0200

    Update BinaryConverter.java

--- BinaryConverter.java
+++ BinaryConverter.java
@@ -7,7 +7,6 @@ public class Main {
         Scanner console = new Scanner(System.in);
         int number = Integer.parseInt(console.nextLine());
         ArrayDeque<Integer>stack  = new ArrayDeque<>();
-
         while (number>0){
             int reminder = number%2;
             stack.push(reminder);
