79a0384f010f04955c7afb4a2ca642af07e6ed31
Radi Radev <33155474+RSRadev@users.noreply.github.com>
2019-11-10 22:20:18 +0200

    Update lamp.pl

--- lamp.pl
+++ lamp.pl
@@ -1,34 +1,33 @@
-#!/usr/bin/perl
+!/usr/bin/perl -wT
 
+use strict; 
 #The basic data types in perl are scalars, arrays, hashes, file handles, type globs, and subroutines.
 
-$long_string_scalar = <<EOT;
+my $long_string_scalar = <<EOT;
 Thi string 
 can be on multiple 
 liens
 EOT
 
-print"$long_string_scalar";
+print"$long_string_scalar\n";
+
 
 #Using subroutine with reference ;)
 
 sub print_msg  {
-
         my ($msg) = @_; 
         print "MSG:$msg\n"; 
         return();
-        
 }
-$ref = \&print_msg;
+my $ref = \&print_msg;
 
 print $ref->("Hello world!");
 
 print $ref->("Ops I use it again :)))"); 
 
-#Perlish switch-case alsol known as a dispathc table
+#Perlish switch-case alsol known as a dispatch table
 
-$ref = {
-
+$ref = { 
         'add'           =>sub{my ($val1, $val2 )=@_; return $val1 + $val2},
         'subtract'      =>sub{my ($val1, $val2 )=@_; return $val1 - $val2},
         'multiply'      =>sub{my ($val1, $val2 )=@_; return $val1 * $val2}
@@ -41,4 +40,9 @@ for my $op (qw(multiply add subtract)){
 
 #with "ref" we see the type of refeference 
 
-print "ref $ref\n";
+
+my @arr =qw (1 2 3 4);
+
+print map{"current value doubled: $_*2\n"} @arr;
+
+my @src  =qw(a b c);
