9bd2467c0ab128beb4986302fe27d383010ad8f5
Radi Radev <33155474+RSRadev@users.noreply.github.com>
2019-11-11 09:36:31 +0200

    Update lamp.pl

--- lamp.pl
+++ lamp.pl
@@ -12,14 +12,16 @@ EOT
 
 print"$long_string_scalar\n";
 
-
 #Using subroutine with reference ;)
 
 sub print_msg  {
+
         my ($msg) = @_; 
         print "MSG:$msg\n"; 
         return();
+
 }
+
 my $ref = \&print_msg;
 
 print $ref->("Hello world!");
@@ -29,18 +31,21 @@ print $ref->("Ops I use it again :)))");
 #Perlish switch-case alsol known as a dispatch table
 
 $ref = { 
+
         'add'           =>sub{my ($val1, $val2 )=@_; return $val1 + $val2},
         'subtract'      =>sub{my ($val1, $val2 )=@_; return $val1 - $val2},
         'multiply'      =>sub{my ($val1, $val2 )=@_; return $val1 * $val2}
+
 };
 
 for my $op (qw(multiply add subtract)){
+               
         my $val = $ref->{$op}->(4,3);
         print "Equals to: $val\n"; 
+
 }
 
 #with "ref" we see the type of refeference 
-
 
 my @arr =qw (1 2 3 4);
 
@@ -55,9 +60,11 @@ print "@scrap\n";
 #perlish hash printing
 
 my %myhash = (
+
         radi=>'programmer',
         ivan=>'doctor',
         john=>'musician'
+
 );
 
 print map {"His name is $_ and he's $myhash{$_}.\n"} keys %myhash; #alsol can be make with values :)))
@@ -75,6 +82,7 @@ my @array2 = (1,2,3);
 print "key of new hash is $_ and the corresponding value is $hash{$_}.\n" for sort keys %hash;
 
 print map {"The key of new hash is $_ and the corresponding value is $hash{$_}.\n"} sort keys %hash;
+
 my @array = sort keys %hash, sort values %hash;
 
 print "@array\n";
