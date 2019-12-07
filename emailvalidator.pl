2937dcdbe057ed1f12abb385c6f0fa68cefceb80
Radi Radev <33155474+RSRadev@users.noreply.github.com>
2019-12-07 10:22:45 +0200

    Update emailvalidator.pl

--- emailvalidator.pl
+++ emailvalidator.pl
@@ -7,5 +7,6 @@ my $email = <STDIN>;
 if ( $email =~ /^[a-zA-Z][\w\.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\w\.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z\.]*[a-zA-Z]$/ ) {
 	print "Valid!\n";
 } else {
+	
 	print "Not valid!\n";
 }
