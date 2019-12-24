a66446ae117422d6b1992e80e580b248ab7e308d
rrraddy <raddy.radev@gmail.com>
2019-12-25 00:49:15 +0200

    script ch name, dir, grouop etc.

---
+++ chuser.pl
@@ -0,0 +1,12 @@
+#!/usr/local/bin/perl
+
+#print"Process strated!Please wait!\n"; 
+
+#`pw usermod -n 'oldname' -l 'newname' -d /usr/home/raddy`;
+
+
+#`pw groupmod -n wheel -l wheel`;
+#`mv /usr/home/oldname /usr/home/newname`
+#`chown -R wheel:wheel /usr/home/raddy`;
+
+#print "Process completed!!!"
