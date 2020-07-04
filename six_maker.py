1b4c92e91ce7d1ce062b1c3064272c82756398ce
raddysr <raddy.radev@gmail.com>
2020-07-04 19:19:33 +0300

    make six from three of each number one to zero(000=6, 111=6, 222=6 etc)

---
+++ six_maker.py
@@ -0,0 +1,24 @@
+import math
+zero = math.factorial(math.factorial(
+    0) + math.factorial(0) + math.factorial(0))
+one = math.factorial(math.factorial(1) + math.factorial(1) + math.factorial(1))
+two = 2 * 2 + 2
+three = 3 * 3 - 3
+four = (4 + 4)-math.sqrt(4)
+five = 5 + (5 / 5)
+six = (6 * 6) / 6
+seven = 7 - (7 / 7)
+eight = 8 - math.sqrt(math.sqrt((8 + 8)))
+nine = (math.sqrt(9) * math.sqrt(9))-math.sqrt(9)
+ten = math.factorial(math.sqrt(10-(10/10)))
+print(zero)
+print(one)
+print(two)
+print(three)
+print(four)
+print(five)
+print(six)
+print(eight)
+print(seven)
+print(nine)
+print(ten)
