8605060513382e4e57dabc9486efc6994c68d04f
Radi Radev <33155474+raddysr@users.noreply.github.com>
2020-12-22 22:12:06 +0200

    Update card_war_games.py

--- card_war_games.py
+++ card_war_games.py
@@ -3,7 +3,6 @@ from random import shuffle
 SUITE = 'H D S C'.split()
 RANKS = '2 3 4 5 6 7 8 9 10 J Q K A'.split()
 
-
 class Deck():
     
     def __init__(self):
@@ -15,6 +14,7 @@ class Deck():
     def split_in_half(self):
         return(self.all_cards[:26], self.all_cards[26:])
 
+    
 class Hand:
     
     def __init__(self,cards):
