caaebec284dfe2c5e34027ad8b1a1cc0fabc7cee
Radi Radev <33155474+raddysr@users.noreply.github.com>
2021-01-18 17:29:59 +0200

    Update rubberduck_test.py

--- rubberduck_test.py
+++ rubberduck_test.py
@@ -2,12 +2,12 @@ def is_anagram(word1, word2):
     arr_w1 = []
     arr_w2 = []
 
-## First let's check if the strings length is equal if it's not we don't need to proceed...
+# First let's check if the strings length is equal if it's not we don't need to proceed...
     if len(word1) != len(word1):
         return False
 
-#Second, I think the easiest way is to turn strings to arrays then just lowercase(to avoid case differneces) 
-#and sort them 
+# Second, I think the easiest way is to turn strings to arrays then just lowercase(to avoid case differences) 
+# and sort them
     for letter in range(len(word1)):
         arr_w1.append(word1[letter].lower())
         arr_w2.append(word2[letter].lower())
@@ -15,7 +15,7 @@ def is_anagram(word1, word2):
     arr_w1.sort()
     arr_w2.sort()
     
-## Then just do the comparing 
+# Then just do the comparing 
     if arr_w1 == arr_w2:
         return(True)
     else:
