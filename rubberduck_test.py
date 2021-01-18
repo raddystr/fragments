54a453d4d72a6898312777ec62044fda5b02a368
Radi Radev <33155474+raddysr@users.noreply.github.com>
2021-01-18 17:07:00 +0200

    Create rubberduck_test.py

---
+++ rubberduck_test.py
@@ -0,0 +1,26 @@
+def is_anagram(word1, word2):
+    arr_w1 = []
+    arr_w2 = []
+
+## First let's check if the strings length is equal if it's not we don't need to proceed...
+    if len(word1) != len(word1):
+        return False
+
+#Second, I think the easiest way is to turn strings to arrays then just lowercase(to avoid case differneces) 
+#and sort them 
+
+    for letter in range(len(word1)):
+        arr_w1.append(word1[letter].lower())
+        arr_w2.append(word2[letter].lower())
+
+    arr_w1.sort()
+    arr_w2.sort()
+## Then just do the comparing 
+    if arr_w1 == arr_w2:
+        return(True)
+    else:
+        return(False)
+
+print(is_anagram('hello', 'hlleo'))
+print(is_anagram('test', 'tast'))
+print(is_anagram('one from me', 'me from one'))
