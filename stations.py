05b5366386adba09b8ac2c0f233ce597304e8d10
Radi Radev <33155474+raddysr@users.noreply.github.com>
2021-04-21 18:15:06 +0300

    Update stations.py

--- stations.py
+++ stations.py
@@ -1,26 +1,14 @@
 def gas_stations(distance, tank_size, stations):
-   
     stations_list = []
-    full_tank = tank_size
-    to_burgas = stations[1:]
-    to_burgas.append(distance)
+    current_distance = tank_size
+    stations.append(distance)
 
     for i in range(len(stations)):
-        #if i == 0:
-            if abs(full_tank - stations[i]) < to_burgas[i] - stations[i]:
-                stations_list.append(stations[i])
-                full_tank = tank_size   
-            else:
-                full_tank -= to_burgas[i] - stations[i]
-        #else:
-        #    if stations[i] - full_tank < to_burgas[i] - stations[i]:
-        #        stations_list.append(stations[
-        # i])
-        #        full_tank = tank_size
-        #    else:
-        #        distance_liters = to_burgas[i] - stations[i]
-        #        full_tank -= distance_liters
+        if stations[i] > current_distance:
+            stations_list.append(stations[i - 1])
+            current_distance = tank_size + stations[i - 1]
     return stations_list
+
 
 print(gas_stations(
 320, 
@@ -31,20 +19,6 @@ print(gas_stations(
 180, 
 220, 
 290]))
-
-
-
-
-print(gas_stations(
-390, 
-80, [
-70, 
-90, 
-140, 
-210, 
-240, 
-280, 
-350]))
             
 
 
