5ebb3ccc21d9a1799d27c81ae84b74b272ea5ec3
Radi Radev <33155474+raddysr@users.noreply.github.com>
2021-11-23 14:26:55 +0200

    Update stor_task.py

--- stor_task.py
+++ stor_task.py
@@ -11,13 +11,17 @@ def rand_choice():
 
 hex_hash = ""
 mail = input("Enter your mail to generate name!")
+
 while not hex_hash.startswith("00"):
     name = ""
+    
     for i in range(5):
         name += rand_choice()
+
     m = md5()
     m.update(name.encode("utf-8"))
     m.update(mail.encode("utf-8"))
     hex_hash = m.hexdigest()
+    
     if  hex_hash.startswith("00"):
         print(f'Name: {name} | Mail: {mail}')
