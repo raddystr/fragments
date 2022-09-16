0759cc90c2dfb417ff91a1bd1a79bfe20e7819cc
Radi Radev <33155474+raddysr@users.noreply.github.com>
2022-09-16 18:20:39 +0300

    Update ec2_remote_script.py

--- ec2_remote_script.py
+++ ec2_remote_script.py
@@ -1,3 +1,14 @@
+'''
+c5 -> type instance
+iop2 -> EBS
+
+lsblk
+sudo -su
+mkdir /sharedfs
+mkfs -t xfs /dev/nvme1n1(run it only once)
+mount /dev/nvme1n1  /sharedfs
+'''
+
 import boto3
 import sys 
 import os
@@ -13,14 +24,17 @@ os.system("aws ec2 start-instances \
 
 time.sleep(120)
 
+
 client.send_command(
     InstanceIds=[
-        'i-0a68fd1ee65e3e636',
+        'i-08b14c8e3a1802d39',
+        'i-0c92a012ed53e2e51'
+
     ],
     DocumentName='AWS-RunShellScript',
     Parameters={
         'commands': [
-            f'python3 /home/ec2-user/test.py "{message}"',
+            f'python3 /home/ec2-user/test.py "TEST OK!!!"',
             'shutdown -h now'
         ]
     }
