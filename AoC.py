fad640271cd6ef7a0d4266fed697a96fc780a4f4
Radi Radev <33155474+raddystr@users.noreply.github.com>
2022-12-01 12:04:32 +0200

    Update and rename test.py to AoC.py

---
+++ AoC.py
@@ -0,0 +1,12 @@
+def max_elven_food_calculator(list_foods: str) -> int:
+    list_calories_per_elf, sums_calories_per_elf, sum_per_elf = list_foods.split("\n"), [], 0
+    
+    for sc in list_calories_per_elf:
+        if sc:
+            sum_per_elf += int(sc)
+            continue
+        sums_calories_per_elf.append(sum_per_elf)
+        sum_per_elf = 0
+        
+    return max(sums_calories_per_elf)
+ 
