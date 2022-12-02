388a1d736810ed4429957fb4d4355b308bdbebce
Radi Radev <33155474+raddystr@users.noreply.github.com>
2022-12-02 15:09:42 +0200

    Update AoC.py

--- AoC.py
+++ AoC.py
@@ -1,13 +1,12 @@
 #AoC day1
 def max_elven_food_calculator(list_foods: str) -> int:
     list_calories_per_elf, sums_calories_per_elf, sum_per_elf = list_foods.split("\n"), [], 0
-    
     for sc in list_calories_per_elf:
         if sc:
             sum_per_elf += int(sc)
             continue
         sums_calories_per_elf.append(sum_per_elf)
         sum_per_elf = 0
-        
-    return max(sums_calories_per_elf)
+        sums_calories_per_elf.sort()
+    return (sums_calories_per_elf[-1], sum(sums_calories_per_elf[-3:]))
  
