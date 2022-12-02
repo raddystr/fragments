a665528a152aa857066fa35b8d529756a4734ce6
Radi Radev <33155474+raddystr@users.noreply.github.com>
2022-12-02 16:41:44 +0200

    Update AoC.py

--- AoC.py
+++ AoC.py
@@ -1,5 +1,5 @@
 #AoC day1
-def max_elven_food_calculator(list_foods: str) -> int:
+def max_elven_food_calculator(list_foods: str) -> tuple:
     list_calories_per_elf, sums_calories_per_elf, sum_per_elf = list_foods.split("\n"), [], 0
     for sc in list_calories_per_elf:
         if sc:
