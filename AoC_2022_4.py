354d2f79331c1ffcd6230f725679c162fd1a37c2
Radi Radev <33155474+raddystr@users.noreply.github.com>
2022-12-04 18:12:26 +0200

    Update AoC_2022_4.py

--- AoC_2022_4.py
+++ AoC_2022_4.py
@@ -4,8 +4,7 @@ def cleaning_optimization(cleaning_area: str, ver: int) -> int:
     redundant = 0
     for area in cleaning_list:
         sub_area_1, sub_area_2 = area.split(',')[0].split('-'), area.split(',')[1].split('-')
-        sub_area_1 = tuple(map(int, sub_area_1))
-        sub_area_2 = tuple(map(int, sub_area_2))
+        sub_area_1, sub_area_2 = tuple(map(int, sub_area_1)), tuple(map(int, sub_area_2))
         if ver == 1: 
             if (sub_area_1[0] <= sub_area_2[0] and sub_area_1[1] >= sub_area_2[1]) or\
             (sub_area_1[0] >= sub_area_2[0] and sub_area_1[1] <= sub_area_2[1]):    
